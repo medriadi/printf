f13f3f144f5b8f59ab621a10259dae75df848c25
medriadi <mohamedriadi@outlook.com>
2023-11-14 01:14:22 +0100

    Small Fixes

--- string_manipulation.c
+++ string_manipulation.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
 * print_from_to - Prints a range of characters from start
-* to stop excluding except.
+*				   to stop excluding except.
 * @start: Starting address of the range.
 * @stop: Stopping address of the range.
 * @except: Address to be excluded from printing.
@@ -10,15 +11,17 @@
 */
 int print_from_to(char *start, char *stop, char *except)
 {
-int sum = 0;
-while (start <= stop)
-{
-if (start != except)
-sum += _putchar(*start);
-start++;
+	int sum = 0;
+
+	while (start <= stop)
+	{
+		if (start != except)
+			sum += _putchar(*start);
+		start++;
+	}
+	return (sum);
 }
-return (sum);
-}
+
 /**
 * print_rev - Prints a string in reverse.
 * @ap: The string to print.
@@ -28,18 +31,22 @@ return (sum);
 */
 int print_rev(va_list ap, params_t *params)
 {
-int len, sum = 0;
-char *str = va_arg(ap, char *);
-(void)params;
-if (str)
-{
-for (len = 0; str[len]; len++)
-;
-for (; len > 0; len--)
-sum += _putchar(str[len - 1]);
+	int len, sum = 0;
+
+	char *str = va_arg(ap, char *);
+
+	(void)params;
+
+	if (str)
+	{
+		for (len = 0; str[len]; len++)
+			;
+		for (; len > 0; len--)
+			sum += _putchar(str[len - 1]);
+	}
+	return (sum);
 }
-return (sum);
-}
+
 /**
 * print_rot13 - Prints a string in ROT13 encoding.
 * @ap: The string to print.
@@ -49,22 +56,26 @@ return (sum);
 */
 int print_rot13(va_list ap, params_t *params)
 {
-int i, index, count = 0;
-char arr[] =
-"NOPQRSTUVWXYZABCDEFGHIJKLM nopqrstuvwxyzabcdefghijklm";
-char *a = va_arg(ap, char *);
-(void)params;
-for (i = 0; a[i]; i++)
-{
-if ((a[i] >= 'A' && a[i] <= 'Z') || (a[i] >= 'a' && a[i] <= 'z'))
-{
-index = a[i] - 65;
-count += _putchar(arr[index]);
-}
-else
-{
-count += _putchar(a[i]);
-}
-}
-return (count);
+	int i, index, count = 0;
+
+	char arr[] =
+
+		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+	char *a = va_arg(ap, char *);
+
+	(void)params;
+
+	for (i = 0; a[i]; i++)
+	{
+		if ((a[i] >= 'A' && a[i] <= 'Z') || (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			index = a[i] - 65;
+			count += _putchar(arr[index]);
+		}
+		else
+		{
+			count += _putchar(a[i]);
+		}
+	}
+	return (count);
 }
